04787aa8ceb3da41136b124384ced2104271bcb8
buraksensoz <buraksensoz@gmail.com>
2022-05-13 08:59:45 +0300

    Add project files.

---
+++ BlazorApp/Pages/Calculate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp.Pages
+{
+    public class CalculateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
